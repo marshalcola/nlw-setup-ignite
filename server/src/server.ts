80cead6140f53c91f330f61fab66b73a7e5abc2b
Marshal <marshalcola@gmail.com>
2023-01-31 08:48:03 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+// Back-end API RESTful
+// localhost:3333/habits
+import Fastity from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
+
+const app = Fastity()
+
+app.register(cors)
+app.register(appRoutes)
+
+app.listen({
+  port:3333,
+}).then(() => {
+  console.log('HTTP Server running!')
+})
